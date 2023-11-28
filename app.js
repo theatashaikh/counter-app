93fbc6b4b1e70c788429bb1c9c679f7d4a43e5be
theatashaikh <atashaikh2000@gmail.com>
2023-11-28 12:48:08 +0530

    Add app.js file and implement counter increment functionality.

---
+++ app.js
@@ -0,0 +1,11 @@
+const countValue = document.querySelector(".count-value");
+const incrementCountBtn = document.querySelector(".increment-count-btn");
+const resetCountBtn = document.querySelector(".reset-count-btn");
+const currentYear = document.querySelector(".current-year");
+
+let counter = 0;
+
+incrementCountBtn.addEventListener("click", () => {
+  counter++;
+  countValue.innerText = counter;
+});
